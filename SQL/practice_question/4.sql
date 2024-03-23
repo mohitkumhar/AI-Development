25017b62de6053f954577b1c819195514b8bc96a
Mohit Kumhar <mohitmolela@gmail.com>
2024-03-23 11:25:11 +0530

    question 4

---
+++ SQL/practice_question/4.sql
@@ -0,0 +1,7 @@
+-- 4. Using WHERE Clause
+--    
+-- Write a SQL query to select all columns from the `orders` table where the `order_date` is '2023-01-01'
+
+USE practice;
+
+SELECT * FROM orders WHERE order_date='2023-01-01';
