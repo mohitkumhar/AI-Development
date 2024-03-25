8b6a77785e5c99d0d69c0aa390c5efbe3fcb74b5
Mohit Kumhar <mohitmolela@gmail.com>
2024-03-25 11:25:11 +0530

    question 3

---
+++ SQL/practice_question/3.sql
@@ -0,0 +1,10 @@
+-- 3. Ordering Results
+--
+-- Write a SQL query to select all columns from the `products` table and order the results by `price` in descending order.
+
+
+USE practice;
+
+SELECT * FROM products ORDER BY price DESC;
+
+
