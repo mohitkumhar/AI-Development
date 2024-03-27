faa60db789c0d7fab10067c65b40cd8a77034082
Mohit Kumhar <mohitmolela@gmail.com>
2024-03-27 11:25:11 +0530

    question 2

---
+++ SQL/practice_question/2.sql
@@ -0,0 +1,6 @@
+-- 2. Filtering Rows
+--    
+--    Write a SQL query to find all the employees in the `employees` table whose `salary` is greater than 50000.
+
+USE practice;
+SELECT * FROM employees WHERE salary > 50000;
