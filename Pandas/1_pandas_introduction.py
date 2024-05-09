79dc9b718e2b8d4f96a9703fb0f1c0fe17054bd3
Mohit Kumhar <mohitmolela@gmail.com>
2024-05-09 18:15:09 +0530

    Pandas Introduction

---
+++ Pandas/1_pandas_introduction.py
@@ -0,0 +1,32 @@
+# What is Pandas ?
+# Pandas is a Python library used for working with data sets
+
+# It has functions for analyzing cleaning, exploring, and manipulating data
+
+# The name "Pandas" has a reference to both "Panel Data" and "Python Data Analysis"
+
+
+# Why Use Pandas ?
+# Pandas allow us to analyze big data and make conclusions based on statistical theories
+
+# Pandas can clean messy data sets, and make them readable and relevant.
+# Relevant data is very important in data science.
+
+
+# What Can Pandas do ?
+# Pandas gives you answer about the data.
+
+# Like:
+#     Is there a correlation between two or more columns?
+
+#     What is average Value?
+
+#     Max Value?
+
+#     Min Value?
+
+# Pandas are also able to delete rows that are not relevant, or contains wrong values, like empty or NULL values. This is called cleaning the data
+
+
+import pandas as pd
+print(pd.__version__)   # 2.2.2
