eb97e54e9909f6547c7d5a504749c9b7f8cdba64
Mohit Kumhar <mohitmolela@gmail.com>
2024-05-16 21:03:57 +0530

    Cleaning Wrong Format

--- Data Manuplation/Pandas/8_cleaning_wrong_format.py
+++ Data Manuplation/Pandas/8_cleaning_wrong_format.py
@@ -1,6 +1,6 @@
 # data in a wrong format: to fix this problem, there are 2 ways: removing the rows or convert all the cell in the same format
 
-# to convert all the cells in the data column into dates. via to_datatime()
+# to convert all the cells in the data column into dates. via to_datetime()
 import pandas as pd
 x = pd.read_csv('data.csv')
 x['Date'] = pd.to_datetime(x['Date'])    # it changes 20201226 to 2020/12/26
